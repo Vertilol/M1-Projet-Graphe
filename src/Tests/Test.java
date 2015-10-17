c65a0db0c19ba63c75a68f1e3390f93171c7384e
Vladimir Karassouloff <vkarassouloff@hotmail.fr>
2015-10-17 21:08:22 +0800

    fazef

--- src/Tests/Test.java
+++ src/Tests/Test.java
@@ -3,7 +3,7 @@ package Tests;
 /**
  * Created by Clement on 17/10/2015.
  */
-public class Test {
+public class Test { // test commit
     public static void main(String[] args){
         String s = "7: [3, 4, 5, 6]";
         String s2 = s.split(":")[1].trim();
