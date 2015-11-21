c5a4f88581b80cafcc760997fc23fb71953ea31b
Vladimir Karassouloff <vkarassouloff@hotmail.fr>
2015-11-21 14:06:18 +0100

    point degen

--- src/Modele/Graphe.java
+++ src/Modele/Graphe.java
@@ -2,17 +2,23 @@ package Modele;
 
  import Exception.ExceptionNoSuchPoint;
 
+ import java.util.ArrayList;
+ import java.util.List;
+
 /**
  * Created by Vladimir on 17/10/2015.
  */
 public class Graphe {
     private Point[] points;
+    private List<Face> faces;
 
     public Graphe(int taille){
         points = new Point[taille];
         for(int i = 0 ; i < taille ; i++){
             points[i] = new Point(String.valueOf(i));
         }
+        faces = new ArrayList<Face>();
+
     }
     public Point getPoint (int num) throws ExceptionNoSuchPoint{
         if(num < 0 || num >= points.length)
@@ -47,6 +53,43 @@ public class Graphe {
             p.setEtatParcoursNonAtteint();
             p.setParent(null);
         }
+    }
+
+    public void addFace(Face f){
+        this.faces.add(f);
+    }
+
+    //retourne liste des points étant placé dans une face
+    public List<Point> getPointsInFace(){
+        List<Point> lp = new ArrayList<Point>();
+        for(Face f : this.faces){
+            for(Point p : f.points){
+                if(!lp.contains(p)){
+                    lp.add(p);
+                }
+            }
+        }
+        return lp;
+    }
+
+    //retourne liste de points n'étant pas dans une face et ayant un de leur voisins dans une face
+    public List<Point> getNonDegeneratedPoints(){
+        List<Point> lp = new ArrayList<Point>();
+        List<Point> pointFace = getPointsInFace();
+        for(Point p : this.points){
+            if(! pointFace.contains(p)){
+                boolean hasNeighborInFace = false;
+                for(Point pTest : p.voisins){
+                    if(pointFace.contains(pTest)){
+                        hasNeighborInFace = true;
+                        break;
+                    }
+                }
+                if(hasNeighborInFace)
+                    lp.add(p);
+            }
+        }
+        return lp;
     }
 
     public int getTaille(){return points.length;}
